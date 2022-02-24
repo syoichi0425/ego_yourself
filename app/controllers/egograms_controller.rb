f9dbdf871769fb3f6357eafa3bec951217df294a
syoichi0425 <syoichi0425@yahoo.co.jp>
2022-02-25 08:25:37 +0900

    エゴグラムの履歴一覧作成の為にコメント記載とroutesのリファクタリング

--- app/controllers/egograms_controller.rb
+++ app/controllers/egograms_controller.rb
@@ -4,8 +4,8 @@ class EgogramsController < ApplicationController
   end
 
   def test
-    @toatl_cp=params[:q_cp1].to_i+params[:q_cp2].to_i
-    @toatl_np=params[:q_np1].to_i+params[:q_np2].to_i
+    @total_cp=params[:q_cp1].to_i+params[:q_cp2].to_i
+    @total_np=params[:q_np1].to_i+params[:q_np2].to_i
 
     #params[:オブジェクト名(テーブルのカラム名など)]
 
@@ -14,40 +14,62 @@ class EgogramsController < ApplicationController
 
 
   def create
-    @cp_toatl=params[:q_cp1].to_i+params[:q_cp2].to_i
-    @np_toatl=params[:q_np1].to_i+params[:q_np2].to_i
+    @cp_total=params[:q_cp1].to_i+params[:q_cp2].to_i
+    @np_total=params[:q_np1].to_i+params[:q_np2].to_i
     #各params合計値
 #    createメソッドに渡す引数のデータ -->
 #   モデル名.create({ カラム名: 値 })
 
-    @cp = EgogramCpQuetion.create(title: @cp_toatl)
-    @np = EgogramNpQuetion.create(title: @np_toatl)
+    @cp = EgogramCpQuetion.create(title: @cp_total)
+    @np = EgogramNpQuetion.create(title: @np_total)
     #↓次作るイメージ2022/02/23
-    #@total_sum = EgogramToatlResult.create(cp_toatl: @cp_toatl,np_toatl: @np_toatl)
+    #@total_sum = EgogramtotalResult.create(cp_total: @cp_total,np_total: @np_total)
+
+
+
+
     #  if @cp.save
-   #     flash[:success] = "Object successfully created"
-
-    #   if
-    #     @cp_>10 && @np>10 && @a >10 && @ac>10 && @fc>10
-    #       redirect_to 結果1のページ
-    #   elsif
-    #     @cp_>10 && @np>10 && @a >10 && @ac>10 && @fc>10
-    #       redirect_to 結果2のページ
-    #   end
-
-    #else
-     # flash[:error] = "Something went wrong"
-     # render 'new'
+    #     flash[:success] = "Object successfully created"
+    # render "result"
+        #  ここでやりたいこと
+          #解答後の画面移行時は、一番最新のEgo_resultモデル(仮)のEgo_result_idを呼び出す
+          #最新のEgo_result_idに紐づけた
+          #結果の各点数に応じたEgo_resultモデル(仮)の該当するカラムや実際に解答点数(CP~FCの5項目)を呼び出す
+          #呼び出した各数値を折線グラフに反映
+          #これらのパラメータを保持してegogram/result.html.erbに飛んで画面表示
+    #  else
+    # flash[:error] = "Something went wrong"
+    # render 'new'
     #end
   end
 
-
+  def result
+  #
+    @egogram_type= "#ここに〇〇型、「××」タイプが入る"
+      #結果のモデルのtypeカラム
+    @user_name="#「ここにユーザー名」"
+      #User.namefind(params[:id])
+    @seikaku="基本の性格"
+      #結果のモデルのseikakuカラム
+      #
+    @strengths="#ここに各結果に基づいた[長所]の文言が入る"
+      #結果のモデルのstrengthsカラム
+    @weakness="#ここに各結果に基づいた[短所]の文言が入る"
+      #結果のモデルのweaknessカラム
+    @always_thinking="#ここに各結果に基づいた[思っていること]の文言が入る"
+      #結果のモデルのalways_thinkingカラム
+    @your_around="#ここに各結果に基づいた[周りから見たあなた]の文言が入る"
+      #結果のモデルのyour_aroundカラム
+  end
     #@test=EgogramCpQuetion.new
     #@test_cp=EgogramCpQuetion.select("title")
     #@test_np=EgogramNpQuetion.select("title")
 
-  def result
+  def history_list
+    #ここにエゴグラムを解答して保存したモデルとカラムの一覧を表示
+    #モデル名.allで取得し表示
   end
+
 
   def confirmation
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
-  resources :goals, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+  resources :contacts, only: [:new, :create]
   resources :diaries, only: [:new,:create]
-  get 'requests/password_forget_request'
-  get 'requests/password_forget_resetting'
-  get 'requests/withdrawal'
-  get 'requests/after_withdrawal'
+  resources :goals, only: [:index, :show, :new, :create, :edit, :update, :destroy]
 
   devise_for :users
+
   root 'contents#index'
   get 'contents/show'
   get "contents/home"
@@ -18,34 +16,34 @@ Rails.application.routes.draw do
   get 'contents/egogram_example'
   get 'contents/diary_explanation'
 
-  post "/diaries",to: "diaries#create"
+  get 'done', to: 'contacts#done', as: 'done'
+  post 'contacts/confirm', to: 'contacts#confirm', as: 'confirm'
+  post 'contacts/back', to: 'contacts#back', as: 'back'
+
   get "diaries/new"
+  post "/diaries",to: "diaries#create"
+
 
 
   get 'egograms/test'
   get 'egograms/test2'
- # get 'egograms/result'
+  get 'egograms/result'
   get 'egograms/confirmation'
+  get 'egograms/history_list
   post "/egograms/test",to: "egograms#create"
-
 
   get "goals/index"
   get "goals/show"
 #  get "goals/:id", to: "goals#show"
   get 'goals/new'
   get "goals/edit"
-
   post "/goals",to: "goals#create"
 
 #  get 'goals/fix_and_dalete'
 
-  resources :contacts, only: [:new, :create]
-  post 'contacts/confirm', to: 'contacts#confirm', as: 'confirm'
-  post 'contacts/back', to: 'contacts#back', as: 'back'
-  get 'done', to: 'contacts#done', as: 'done'
+  get 'requests/password_forget_request'
+  get 'requests/password_forget_resetting'
+  get 'requests/withdrawal'
+  get 'requests/after_withdrawal'
 
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
