42917bbe783c51a5f072e95e219ed36fa0c45600
syoichi0425 <syoichi0425@yahoo.co.jp>
2022-02-28 13:59:53 +0900

    リファクタリング

--- app/controllers/egograms_controller.rb
+++ app/controllers/egograms_controller.rb
@@ -1,6 +1,6 @@
 class EgogramsController < ApplicationController
   before_action :authenticate_user!
-
+  before_action :ego_quetions_create_params,only: [:create]
   before_action :ego_result_params,only: [:index,:result]
   before_action :ego_result_to_page_params,only: [:index,:result]
 
@@ -11,67 +11,22 @@ class EgogramsController < ApplicationController
   end
 
   def index
-    #ここにエゴグラムを解答して保存したモデルとカラムの一覧を表示
-    #EgogramNpQuetionは仮で置いてるだけ
-    #@index=EgoScore.all
+#未実装：ここにエゴグラムを解答して保存したモデルとカラムの一覧を表示
+#@index=EgoScore.all
 #EgoScore.where(user_id: current_user.id)でEgoScoreのuser_idカラムを
 #ログインユーザーのuser_idだけを絞っている
 #index=EgoScore.where(user_id: current_user.id).last
-
-
-
-#cp=index.pluck(:cp_score).last
-#np=index.pluck(:np_score).last
-  #  @iindex=EgoScore.where(user_id: current_user.id).last
-
-
-#  User.where(id: [1,2,3]).pluck(:id, :name)
-
-#@index=index.np_score
-
-#@cp=index.pluck(:cp_score)
-#@np=index.pluck(:cp_score).last
-#@a=index.pluck(:cp_score).last
-#@fc=index.pluck(:cp_score).last
-#@ac=index.pluck(:cp_score).last
-
-
-# cp=EgoScore.pluck(:cp_score).last
-# np=EgoScore.pluck(:np_score).last
-
-# #最新のResult_idを読み込んで
-# #カラムcp~acの5つを呼び出し
-# # cp=newest_result_id(:cp_score).to_i
-# # np=newest_result_id(:np_score).to_i
-# # a=newest_result_id(:a_score).to_i
-# # fc=newest_result_id(:fc_score).to_i
-# # ac=newest_result_id(:ac_score).to_i
-
-# # #ifの各条件に分けてresultテーブルのidを決定する
-#  if cp<4 && np<5
-#   @result=TestResult.where(id: 1)
-# # elsif cp<10 && np>10
-# #  elsif cp<3 && np>3
-# #   @result=TestResult.where(id: 2)
-# #    #&& np<10 && a<10 && fc<10 && ac<10
-# # # #ifで決定したidをResultのidに代入し、result.htmlに値を渡す
-#   else
-#    @result=TestResult.where(id: 2)
-
-#  end
-
 
    # @history=EgogramNpQuetion.where(user_id: current_user.id)
   end
 
   def test
+
     @cp_total=params[:q_cp1].to_i+params[:q_cp2].to_i
     @np_total=params[:q_np1].to_i+params[:q_np2].to_i
     @a_total=params[:q_a1].to_i+params[:q_a2].to_i
     @fc_total=params[:q_fc1].to_i+params[:q_fc2].to_i
     @ac_total=params[:q_ac1].to_i+params[:q_ac2].to_i
-    #params[:オブジェクト名(テーブルのカラム名など)]
-
 
   end
 
@@ -81,27 +36,14 @@ def test2
   @a=ego
 end
 
-
-
-  def create
-
-    @cp_total=params[:q_cp1].to_i+params[:q_cp2].to_i
-    @np_total=params[:q_np1].to_i+params[:q_np2].to_i
-    @a_total=params[:q_a1].to_i+params[:q_a2].to_i
-    @fc_total=params[:q_fc1].to_i+params[:q_fc2].to_i
-    @ac_total=params[:q_ac1].to_i+params[:q_ac2].to_i
-
-#@egoscore=[@cp_total,@cp_total,@cp_total,@cp_total,@cp_total,]
-
-
-    #各params合計値
-#    createメソッドに渡す引数のデータ
-#   モデル名.create({ カラム名: 値 })
-
-#    EgoScore.create
+#--------------createについて----------------
 #createメソッドは、newとsaveを同時に行っているメソッド
 #=生成したインスタンスを使って何かしらの操作を行いたい場合には、createメソッドは使用できない
 #そのため、newメソッドとsaveメソッドで操作する
+#--------------createについて----------------
+def create
+#createアクション:egograms/test.html.erbのラジオボタンのparametersを取得して各合計点数を出して、EgoScoreテーブルの各カラムに保存
+#createメソッドに渡す引数の書き方 => モデル名.create({ カラム名: 値 })
   @ego_score=EgoScore.new(
 
           cp_score: @cp_total,
@@ -111,32 +53,15 @@ end
           ac_score: @ac_total,
           user_id: current_user.id)
 
-if  @ego_score.save
-  redirect_to egograms_result_path,notice: "保存しました"
-#redirect_to 〇〇_pathを使って診断結果のページに飛ぶのとその診断の数値をresultテーブルの値と比較してページの生成を行なってそれを画面に表示する
-else
-  render :test ,notice: "失敗しました"
-    end
+  if  @ego_score.save
+    redirect_to egograms_result_path,notice: "保存しました"
+  #redirect_to 〇〇_pathを使って診断結果のページに飛ぶのとその診断の数値をresultテーブルの値と比較してページの生成を行なってそれを画面に表示する
+  else
+  # flash[:error] = "Something went wrong"
+    render :test ,notice: "失敗しました"
   end
-    #↓次作るイメージ2022/02/23
-    #@total_sum = EgogramtotalResult.create(cp_total: @cp_total,np_total: @np_total)
+end
 
-
-
-
-    #  if @cp.save
-    #     flash[:success] = "Object successfully created"
-    # render "result"
-        #  ここでやりたいこと
-          #解答後の画面移行時は、一番最新のEgo_resultモデル(仮)のEgo_result_idを呼び出す
-          #最新のEgo_result_idに紐づけた
-          #結果の各点数に応じたEgo_resultモデル(仮)の該当するカラムや実際に解答点数(CP~FCの5項目)を呼び出す
-          #呼び出した各数値を折線グラフに反映
-          #これらのパラメータを保持してegogram/result.html.erbに飛んで画面表示
-    #  else
-    # flash[:error] = "Something went wrong"
-    # render 'new'
-    #end
 
 def show
 
@@ -145,11 +70,6 @@ end
   def result
 #モデル名.pluck(:カラム名)  全てのデータではなく特定のカラムの値だけ取得
 #EgoScoreの最新を取得している(＝最後に保存したego_score_id)の(https://teratail.com/questions/143372)
-
-
-
-
-
 
     @egogram_type= "#ここに〇〇型、「××」タイプが入る"
       #結果のモデルのtypeカラム
@@ -167,22 +87,27 @@ end
     @people_around_youとは="#ここに各結果に基づいた[周りから見たあなた]の文言が入る"
       #結果のモデルのpeople_around_youカラム
   end
-    #@test=EgogramCpQuetion.new
-    #@test_cp=EgogramCpQuetion.select("title")
-    #@test_np=EgogramNpQuetion.select("title")
-
-
-
 
   def confirmation
   end
 end
 
+
+
+
 private
 
+def ego_quetions_create_params  #ラジオボタンで取得した各paramsの合計値を出すアクション(createアクションで主に使用予定)
+    #params[:オブジェクト名(テーブルのカラム名など)]
+  @cp_total=params[:q_cp1].to_i+params[:q_cp2].to_i
+  @np_total=params[:q_np1].to_i+params[:q_np2].to_i
+  @a_total=params[:q_a1].to_i+params[:q_a2].to_i
+  @fc_total=params[:q_fc1].to_i+params[:q_fc2].to_i
+  @ac_total=params[:q_ac1].to_i+params[:q_ac2].to_i
+end
 
 
-def ego_result_params
+def ego_result_params #EgoScoreから最新の各カラムの値を取得するアクション
 
 # 未実装:ログインユーザーid(user_id)とegoscoreのuser_idが同じかどうかで本人か識別
 # unless EgoScore(params[user_session])==current_user
@@ -199,7 +124,7 @@ def ego_result_params
 
 end
 
-def ego_result_to_page_params
+def ego_result_to_page_params　#def ego_result_paramsと組み合わせるアクションで、EgoScoreから最新の各カラムの値を取得するアクション
 
   #if cp<4 && np<4 && a<4 && fc<4 && ac<4
   if @cp>=0 && @np>=0 && @a>=0 && @fc>=0 && @ac>=0
@@ -215,9 +140,9 @@ def ego_result_to_page_params
    end
 end
 
-
+#ストロングパラメータ(セキュリティ) https://qiita.com/morikuma709/items/2dc20d922409ae7ce216
+#書き方=>params.require(:使用するモデル名).permit(:使用するカラム名)
 def ego_score_params
   params.require(:ego_score).permit(:cp_score,:np_score,:a_score,:fc_score,:ac_score).merge(user_id: current_user.id)
-#params.require(:使用するモデル名).permit(:使用するカラム名)
-end
 
+end
