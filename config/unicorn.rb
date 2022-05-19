9a23bd5f9a942610c3fbcfff154d25c7190d8714
syoichi0425 <syoichi0425@yahoo.co.jp>
2022-05-19 23:55:13 +0900

    やめた：サーバ上でのアプリケーションコードが設置されているディレクトリを変数に入れておく

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,6 +1,6 @@
 #サーバ上でのアプリケーションコードが設置されているディレクトリを変数に入れておく
-# app_path = File.expand_path('../../', __FILE__)
-app_path = File.expand_path('/var/www/ego_yourself', __FILE__)
+app_path = File.expand_path('../../', __FILE__)
+# app_path = File.expand_path('/var/www/ego_yourself', __FILE__)
 
 
 #アプリケーションサーバの性能を決定する
