dc68f973891e7eb4131b5191989f4429ebdd9939
syoichi0425 <syoichi0425@yahoo.co.jp>
2022-07-03 00:01:45 +0900

    ページ改修

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -5,9 +5,11 @@ class SearchesController < ApplicationController
     @range = params[:range]
 
     if @range == "Diary"
-      @diaries = Diary.looks(params[:search], params[:word])
+      #全ユーザーを検索したい場合は下記=> @diaries = Diary.looks(params[:search], params[:word]).order(created_at: :desc)
+      @diaries = Diary.where(user_id: current_user.id).looks(params[:search], params[:word]).order(created_at: :desc)
+
     else
-      @goals = Goal.looks(params[:search], params[:word])
+      @goals = Goal.where(user_id: current_user.id).looks(params[:search], params[:word]).order(created_at: :desc)
     end
   end
 end
